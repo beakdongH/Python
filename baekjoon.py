9f5d281df78d11fbb87801e92bf6e907260d8100
백동흔 <128360692+beakdongH@users.noreply.github.com>
2023-07-31 16:31:00 +0900

    백준 코딩

---
+++ baekjoon.py
@@ -0,0 +1,27 @@
+#2557
+print("Hello World!")
+
+#1000
+a, b = map(int, input().split())
+print(a+b)
+
+#1001
+a, b = map(int, input().split())
+print(a-b)
+
+#10998
+a, b = map(int, input().split())
+print(a*b)
+
+#1008
+a, b = map(int, input().split())
+print(a/b)
+
+#10869
+a, b = map(int,input().split())
+print(a+b)
+print(a-b)
+print(a*b)
+print(a//b)
+print(a%b)
+
