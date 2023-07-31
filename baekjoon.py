0cedd5e3e3f5b2c77ec5055cb9c34333d99ca75b
백동흔 <128360692+beakdongH@users.noreply.github.com>
2023-07-31 16:31:56 +0900

    백준 코딩

--- baekjoon.py
+++ baekjoon.py
@@ -25,3 +25,31 @@ print(a*b)
 print(a//b)
 print(a%b)
 
+#1330
+a, b = map(int,input().split())
+if a>b:
+    print(">")
+if a>b:
+    print("<")    
+if a==b:
+    print("==")    
+
+#9498
+a = int(input())
+if (100>=a>=90):
+    print("A")
+elif (90>a>=80):
+    print("B")
+elif (80>a>=70):
+    print("C")
+elif (70>a>=60):
+    print("D")
+else:
+    print("F")
+  
+#2753
+a = int(input())
+if(a%4==0 and a%100!=0) or a%400==0:
+    print("1")
+else:
+    print("0")
