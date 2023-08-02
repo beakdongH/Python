bd69a5a002db561cc34df7249713dd2de7147543
백동흔 <128360692+beakdongH@users.noreply.github.com>
2023-08-02 22:09:45 +0900

    Update baekjoon.py

--- baekjoon.py
+++ baekjoon.py
@@ -77,3 +77,12 @@ if m - 45<0 :
 elif m-45>=0 :
     m = m-45
 print(h, m)
+
+#14681
+x = int(input())
+y = int(input())
+
+if (x>0 and y>0): print("1")
+elif (x<0 and y>0): print("2")
+elif (x<0 and y<0): print("3")
+else: print("4")
