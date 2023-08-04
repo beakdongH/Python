986bf22b637077133588480d78cb04c21765c98c
백동흔 <128360692+beakdongH@users.noreply.github.com>
2023-08-05 01:29:23 +0900

    CodeUp-py

--- CodeUp100.py
+++ CodeUp100.py
@@ -3,3 +3,82 @@ print("Hello")
 
 //6002
 print("Hello World")
+
+//6003
+print("Hello") 
+print("World")
+
+//6004
+print("'Hello'");
+
+//6005
+print('"Hello World"');
+
+//6006
+print("\"!@#$%^&*()\'")
+
+//6007
+print("\"C:\\Download\\\'hello\'.py\"")
+
+//6008
+print("print(\"Hello\\nWorld\")")
+
+//6009
+a = input()
+print(a)
+
+//6010
+n = input()
+n = int(n)
+print(n)
+
+//6011
+f = input()
+f = float(f)
+print(f)
+
+//6012
+a = input() 
+b = input()
+a=int(a)
+b=int(b)
+print(a)
+print(b)
+
+//6013
+a = str(input()) 
+b = str(input())
+print(b)
+print(a)
+
+//6014
+f = input()
+f = float(f)
+print(f)
+print(f)
+print(f)
+
+//6015
+a, b = input().split()
+a=int(a)
+b=int(b)
+print(a)
+print(b)
+
+//6016
+a,b = input().split()
+a=str(a)
+b=str(b)
+print(b,a)
+
+//6017
+s = input()
+print(s, s, s)
+
+//6018
+a, b = input().split(':')
+print(a, b, sep=':')
+
+//6019
+a, b,c = input().split('.')
+print(c, b,a, sep='-')
