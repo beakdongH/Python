1e0eff0da266033725c875f3419b0896af4f04fa
백동흔 <128360692+beakdongH@users.noreply.github.com>
2023-09-20 23:11:22 +0900

    Update mathAI.py

--- mathAI.py
+++ mathAI.py
@@ -104,3 +104,30 @@ A = np.matrix([[0,0,0],
 
 print(A*T)
 print(T*A)
+
+
+import numpy as np
+
+def perc(X,W,d):
+  X = np.array(X)
+  W = np.array(W)
+  S = np.dot(X,W) #dot연산 : 내적, 선형결합
+  if S>=d:
+    return(1)
+  else:
+    return(0)
+
+def perc_and(X):
+  return(perc(X,[5,5],6))
+def perc_or(X):
+  return(perc(X,[5,5],4))
+def perc_nand(X):
+  return(1-perc_and(X))
+def perc_xor(X):
+  return(perc_and([perc_nand(X),perc_or(X)]))
+
+
+
+for x in range(2):
+  for y in range(2):
+    print(perc_xor([x,y]))
